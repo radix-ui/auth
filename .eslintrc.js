f4efccb79dddc211583c74bc42c5f6fb63d1a8ca
Benoît Grélard <benoit.grelard@gmail.com>
2023-03-13 11:17:52 +0000

    Add storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['react-app', 'plugin:jsx-a11y/recommended'],
+  extends: ['react-app', 'plugin:jsx-a11y/recommended', 'plugin:storybook/recommended'],
   plugins: ['jsx-a11y'],
   rules: {
     'react/jsx-pascal-case': [2, { allowNamespace: true }],
---
+++ src/sign-up.stories.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as SignUp from './sign-up';
+
+export default { title: 'Components/SignUp' };
+
+export const Basic = () => <SignUp.Submit>sdfsd</SignUp.Submit>;
